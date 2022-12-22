2c450a15eb681033d81d78c7fb9cf97f3fe0c06f
mahsahmd <m.ahmadpoor@hamipishgaman.com>
2022-12-22 18:06:38 +0330

    fix home layout

--- src/components/Screen/Home/index.jsx
+++ src/components/Screen/Home/index.jsx
@@ -5,13 +5,7 @@ import News from "./News";
 const Home = () => {
 	return (
 		<>
-			<StyledDiv>
-				<StyledCustomDivider>
-					<StyledTitle textAlign="center" fontSize={{ md: "45px", xs: "20px" }} fonWeight="400">
-						Last product
-					</StyledTitle>
-				</StyledCustomDivider>
-			</StyledDiv>
+
 			<LastProduct />
 			<StyledDiv mt="140px">
 				<StyledCustomDivider right>
