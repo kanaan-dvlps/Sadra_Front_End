927735882c27bfc825029beda4952ea1c63c17e5
mahsahmd <m.ahmadpoor@hamipishgaman.com>
2022-12-22 17:42:31 +0330

    add curved vector to banner

--- src/components/Common/Banner/index.jsx
+++ src/components/Common/Banner/index.jsx
@@ -3,29 +3,33 @@ import { StyledContainer } from "./style";
 
 const Banner = () => {
 	return (
-		<StyledContainer>
-			<StyledRow>
-				<StyledCol md={12} xs={24}>
-					<StyledDiv
-						height="100%"
-						display="flex"
-						flexDirection="column"
-						justifyContent="center"
-						textAlign="center"
-					>
-						<StyledTitle fontSize={{ md: "50px", xs: "30px" }} fontWeight="700" color="#002434">
-							SPINE SOLUTIONS
-						</StyledTitle>
-						<StyledTitle fontSize={{ md: "25px", xs: "15px" }} textAlign="center" color="#000000">
-							Dynamishe Schraube
-						</StyledTitle>
-					</StyledDiv>
-				</StyledCol>
-				<StyledCol md={12} xs={24}>
-					<StyledImage width="100%" src="/images/home/heroHeader.svg" />
-				</StyledCol>
-			</StyledRow>
-		</StyledContainer>
+		<>
+			<StyledContainer>
+				<StyledRow>
+					<StyledCol md={12} xs={24}>
+						<StyledDiv
+							height="100%"
+							display="flex"
+							flexDirection="column"
+							justifyContent="center"
+							textAlign="center"
+						>
+							<StyledTitle fontSize={{ md: "50px", xs: "30px" }} fontWeight="700" color="#002434">
+								SPINE SOLUTIONS
+							</StyledTitle>
+							<StyledTitle fontSize={{ md: "25px", xs: "15px" }} textAlign="center" color="#000000">
+								Dynamishe Schraube
+							</StyledTitle>
+						</StyledDiv>
+					</StyledCol>
+					<StyledCol md={12} xs={24}>
+						<StyledImage width="100%" src="/images/home/heroHeader.svg" />
+					</StyledCol>
+				</StyledRow>
+			</StyledContainer>
+			<img src='/images/curved-vector.svg'></img>
+		</>
+
 	);
 };
 
